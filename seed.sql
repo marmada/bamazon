c5685c614ffe7b8176f66e0b1f2d81ab19425a0a
Miriam Armada <miriam_armada@yahoo.com>
2018-04-17 21:58:12 -0700

    update files

---
+++ seed.sql
@@ -0,0 +1,10 @@
+USE bamazon
+;	
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+
+values ('
+Harry Potter Paperback Box Set','Books', 52.99, 98);
+;
+
+SELECT * from products;
+
